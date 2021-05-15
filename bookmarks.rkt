1caa1760cb6a14a6a0c44062d3c67054dafc182b
Andrés <duquedeturing@gmail.com>
2021-05-15 16:16:25 -0600

    added links

--- bookmarks.rkt
+++ bookmarks.rkt
@@ -2,13 +2,13 @@
 
 (struct tag (name) #:transparent)
 
-(struct bookmark (title url tags) #:transparent)
+(struct bookmark (title url tags comment) #:transparent)
 
 (define bookmarks '())
 
 
-(define (b title url tags)
-  (define newbk (bookmark title url (map tag tags)))
+(define (b title url tags comment)
+  (define newbk (bookmark title url (map tag tags) comment))
   (set! bookmarks (cons newbk bookmarks)))
 
 (define (u a)
@@ -45,9 +45,19 @@
 
 (define (generate)
   
-  (b "Ursula K. Le Guin" (u "https://www.ursulakleguin.com/home/") '(watchdog ❤))
-  (b "Category Theory" (u "https://plato.stanford.edu/entries/category-theory/") '(sep category-theory cs))
-  (b "dmitry.gr"(u "https://dmitry.gr") '(electronics re blog watchdog))
+  (b "Ursula K. Le Guin" (u "https://www.ursulakleguin.com/home/") '(watchdog ❤) "web home")
+  (b "Category Theory" (u "https://plato.stanford.edu/entries/category-theory/") '(sep category-theory cs) "")
+  (b "dmitry.gr"(u "https://dmitry.gr") '(electronics re blog watchdog) "")
+  (b "Saleae Logic" (u "https://www.saleae.com/") '(products electronics reverse-engineering) "logic analyzer recommended in https://dmitry.gr/?r=05.Projects&proj=30.%20Reverse%20Engineering%20an%20Unknown%20Microcontroller")
+  (b "gofeed" (u "https://github.com/mmcdole/gofeed") '(code rss github) "rss feed reader in go")
+  (b "Send My: Arbitrary data transmission via Apple's Find My network" (u "https://positive.security/blog/send-my") '(re protocols privacy security article) "")
+  (b "3 Simple Ways of Programming an ESP8266 12X Module" (u "https://www.instructables.com/3-Simple-Ways-of-Programming-an-ESP8266-12X-Module/") '(electronics howto) "")
+  (b "Getting Started with ESP8266 WiFi Transceiver (Review)" (u "https://randomnerdtutorials.com/getting-started-with-esp8266-wifi-transceiver-review/") '(electronics howto) "")
+  (b "How to Flash ESP-01 Firmware to the Improved SDK v2.0.0" (u "https://www.allaboutcircuits.com/projects/flashing-the-ESP-01-firmware-to-SDK-v2.0.0-is-easier-now/") '(electronics howto) "")
+  (b "Category Theory: Lecture Notes And Online Books" (u "https://www.logicmatters.net/categories/") '(cs category-theory) "")
+  (b "Terminal and initial objects 1" (u "https://youtu.be/yeQcmxM2e5I") '(cs category-theory video) "")
+  (b "Category theory for the working hacker by Philip Wadler " (u "https://youtu.be/V10hzjgoklA") '(cs category-theory video ❤) "")
+
 
   (export
    "readme.md"
--- readme.md
+++ readme.md
@@ -1,5 +1,14 @@
 # My bookmarks
 
+ * [Category theory for the working hacker by Philip Wadler ](https://youtu.be/V10hzjgoklA)
+ * [Terminal and initial objects 1](https://youtu.be/yeQcmxM2e5I)
+ * [Category Theory: Lecture Notes And Online Books](https://www.logicmatters.net/categories/)
+ * [How to Flash ESP-01 Firmware to the Improved SDK v2.0.0](https://www.allaboutcircuits.com/projects/flashing-the-ESP-01-firmware-to-SDK-v2.0.0-is-easier-now/)
+ * [Getting Started with ESP8266 WiFi Transceiver (Review)](https://randomnerdtutorials.com/getting-started-with-esp8266-wifi-transceiver-review/)
+ * [3 Simple Ways of Programming an ESP8266 12X Module](https://www.instructables.com/3-Simple-Ways-of-Programming-an-ESP8266-12X-Module/)
+ * [Send My: Arbitrary data transmission via Apple's Find My network](https://positive.security/blog/send-my)
+ * [gofeed](https://github.com/mmcdole/gofeed)
+ * [Saleae Logic](https://www.saleae.com/)
  * [dmitry.gr](https://dmitry.gr)
  * [Category Theory](https://plato.stanford.edu/entries/category-theory/)
  * [Ursula K. Le Guin](https://www.ursulakleguin.com/home/)
