aac35e0b7ac63682d7b5d1f5befd14600c6f83f1
Andrés <duquedeturing@gmail.com>
2021-05-15 14:08:45 -0600

    html generation

--- bookmarks.rkt
+++ bookmarks.rkt
@@ -1,6 +1,5 @@
 #lang racket
 
-
 (struct tag (name) #:transparent)
 
 (struct bookmark (title url tags) #:transparent)
@@ -18,10 +17,29 @@
 (define (t tl)
   tl)
 
-(define (start)
+(define (html-bookmark bmrk)
+  (string-append "<a href=\"" (bookmark-url bmrk) "\">"
+                 (bookmark-title bmrk)
+                 "</a>" ))
 
+(define (html-bookmarks bmrks)
+  (string-append "<ul>"
+                 (string-join (map (lambda (item)
+                                     (string-append "<li>" item "</li>"))
+                                   (map html-bookmark bmrks)))
+                 "</ul>"))
+
+(define (export html)
+  (define out (open-output-file "bookmarks.html" #:exists 'replace))
+  (display html out)
+  (close-output-port out))
+
+(define (start)
   
   (b "Ursula K. Le Guin" (u "https://www.ursulakleguin.com/home/") '(watchdog ❤))
   (b "Category Theory" (u "https://plato.stanford.edu/entries/category-theory/") '(sep category-theory cs))
 
-  bookmarks)
+  (export
+   (string-append "<html lang='en'><head> <meta charset='utf-8' /><meta name='viewport' content='width=device-width, initial-scale=1' /><title>My Bookmarks</title></head><body>"
+                  (html-bookmarks bookmarks)
+                  "</body></html>")))
